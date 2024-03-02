3b129f1ed5e31751caced050d3b48cd056896387
Xinyi Zhang <42613048+TXyZ2021@users.noreply.github.com>
2024-03-02 00:48:15 -0500

    增加功能：倒计时30秒视觉和音频提示

--- src/DebateTimer.js
+++ src/DebateTimer.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, Fragment} from 'react';
-import sound from './notify.wav';
+import end_sound from './notify.wav';
+import r30_sound from './split.wav';
 
 const DebateTimer = () => {
     const [timeLeft, setTimeLeft] = useState(0);
@@ -9,7 +10,7 @@ const DebateTimer = () => {
     const [runningAff, setRunningAff] = useState(false);
     const [runningNeg, setRunningNeg] = useState(false);
     const [selectedStage, setSelectedStage] = useState('');
-    const [timerTitle, setTimerTitle] = useState('请选择辩论环节');
+    const [timerTitle, setTimerTitle] = useState('测试声音');
     const [isTimeUp, setIsTimeUp] = useState(false);
     const [isAffTimeUp, setIsAffTimeUp] = useState(false);
     const [isNegTimeUp, setIsNegTimeUp] = useState(false);
@@ -48,7 +49,7 @@ const DebateTimer = () => {
 
 
     const debateStages = {
-        '请选择辩论环节': 0,
+        '测试声音': 31,
         '正方一辩发言': 210,
         '反方四辩盘问正方一辩': 90,
         '反方一辩发言': 210,
@@ -72,10 +73,14 @@ const DebateTimer = () => {
             interval = setInterval(() => {
                 setTimeLeft((prevTime) => prevTime - 1);
             }, 1000);
-        } else if (running && timeLeft === 0) {
+        }
+        if (running && timeLeft === 30) {
+            playSound('30');
+        }
+        if (running && timeLeft === 0) {
             setRunning(false);
             setIsTimeUp(true);
-            playSound();
+            playSound('end');
             //alert('时间到！');
         }
         return () => clearInterval(interval);
@@ -87,11 +92,14 @@ const DebateTimer = () => {
             interval = setInterval(() => {
                 setTimeLeftAff((prevTime) => prevTime - 1);
             }, 1000);
-        } else if (runningAff && timeLeftAff === 0) {
+        }
+        if (runningAff && timeLeftAff === 30) {
+            playSound('30');
+        }
+        if (runningAff && timeLeftAff === 0) {
             setRunningAff(false);
             setIsAffTimeUp(true);
-            playSound();
-            //alert('正方时间到！');
+            playSound('end');
         }
         return () => clearInterval(interval);
     }, [runningAff, timeLeftAff]);
@@ -102,11 +110,14 @@ const DebateTimer = () => {
             interval = setInterval(() => {
                 setTimeLeftNeg((prevTime) => prevTime - 1);
             }, 1000);
-        } else if (runningNeg && timeLeftNeg === 0) {
+        }
+        if (runningNeg && timeLeftNeg === 30) {
+            playSound('30');
+        }
+        if (runningNeg && timeLeftNeg === 0) {
             setRunningNeg(false);
             setIsNegTimeUp(true);
-            playSound();
-            //alert('反方时间到！');
+            playSound('end');
         }
         return () => clearInterval(interval);
     }, [runningNeg, timeLeftNeg]);
@@ -133,9 +144,60 @@ const DebateTimer = () => {
         return `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     };
 
-    const playSound = () => {
-        const audio = new Audio(sound);
-        audio.play().then(r => console.log(r)).catch(e => console.log(e));
+    useEffect(() => {
+        let interval;
+        if (selectedStage !== '自由辩论' && selectedStage !== '正方二辩对辩反方二辩') {
+            if (running && timeLeft <= 30) {
+                interval = setInterval(() => {
+                    if (timeLeft === 30) {
+                        document.getElementById('clock').classList.add('time-30s-blinking');
+                    } else if (timeLeft < 27) {
+                        document.getElementById('clock').classList.remove('time-30s-blinking');
+                    }
+                }, 100);
+            }
+            else if (running && timeLeft > 30) {
+                document.getElementById('clock').classList.remove('time-30s-blinking');
+            }
+        }
+        else {
+            if (runningAff && timeLeftAff <= 30) {
+                interval = setInterval(() => {
+                    if (timeLeftAff === 30) {
+                        document.getElementById('clockAff').classList.add('time-30s-blinking');
+                    } else if (timeLeftAff < 27) {
+                        document.getElementById('clockAff').classList.remove('time-30s-blinking');
+                    }
+                }, 100);
+            }
+            else if (runningAff && timeLeftAff > 30) {
+                document.getElementById('clockAff').classList.remove('time-30s-blinking');
+            }
+            if (runningNeg && timeLeftNeg <= 30) {
+                interval = setInterval(() => {
+                    if (timeLeftNeg === 30) {
+                        document.getElementById('clockNeg').classList.add('time-30s-blinking');
+                    } else if (timeLeftNeg < 27) {
+                        document.getElementById('clockNeg').classList.remove('time-30s-blinking');
+                    }
+                }, 100);
+            }
+            else if (runningNeg && timeLeftNeg > 30) {
+                document.getElementById('clockNeg').classList.remove('time-30s-blinking');
+            }
+        }
+        return () => clearInterval(interval);
+    }, [running, runningAff, runningNeg, timeLeft, timeLeftAff, timeLeftNeg]);
+
+    const playSound = (mode) => {
+        if(mode === 'end') {
+            const audio = new Audio(end_sound);
+            audio.play().then(r => console.log(r)).catch(e => console.log(e));
+        }
+        if(mode === '30') {
+            const audio = new Audio(r30_sound);
+            audio.play().then(r => console.log(r)).catch(e => console.log(e));
+        }
     };
 
     useEffect(() => {
@@ -411,7 +473,7 @@ const DebateTimer = () => {
                     <div className='debate-timers-container'>
                         <div className='timer-box'>
                             <h3>正方</h3>
-                            <h1 className={isAffTimeUp ? 'blinking' : ''}>{formatTime(timeLeftAff)}</h1>
+                            <h1 className={isAffTimeUp ? 'blinking' : ''} id='clockAff'>{formatTime(timeLeftAff)}</h1>
                             <div className='controls'>
                                 <button className={!runningAff ? 'active' : ''} onClick={() => setRunningAff(true)} disabled={runningAff}>
                                     ▶️
@@ -429,7 +491,7 @@ const DebateTimer = () => {
                         </div>
                         <div className='timer-box'>
                             <h3>反方</h3>
-                            <h1 className={isNegTimeUp ? 'blinking' : ''}>{formatTime(timeLeftNeg)}</h1>
+                            <h1 className={isNegTimeUp ? 'blinking' : ''} id='clockNeg'>{formatTime(timeLeftNeg)}</h1>
                             <div className='controls'>
                                 <button className={!runningNeg ? 'active' : ''} onClick={() => setRunningNeg(true)} disabled={runningNeg}>
                                     ▶️
@@ -439,7 +501,7 @@ const DebateTimer = () => {
                                 </button>
                                 <button className={!runningNeg ? 'active' : ''} onClick={() => {
                                     setIsNegTimeUp(false);
-                                    setTimeLeftNeg(debateStages[selectedStage])
+                                    setTimeLeftNeg(debateStages[selectedStage]);
                                 }} disabled={runningNeg}>
                                     🔃
                                 </button>
@@ -448,7 +510,7 @@ const DebateTimer = () => {
                     </div>
                 ) : (
                     <div className='timer-box'>
-                        <h1 className={isTimeUp ? 'blinking' : ''}>{formatTime(timeLeft)}</h1>
+                        <h1 className={isTimeUp ? 'blinking' : 'timer'} id='clock'>{formatTime(timeLeft)}</h1>
                         <div className='controls'>
                             <button className={!running ? 'active' : ''} onClick={() => setRunning(true)} disabled={running}>
                                 ▶️

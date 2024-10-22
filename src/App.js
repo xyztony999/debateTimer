a116ddc577fe410573d96bb6e537aaab7212b6cf
Xinyi Zhang <42613048+TXyZ2021@users.noreply.github.com>
2024-10-22 20:07:29 +1100

    将时长和计时器模式与FireBase FireStore数据库同步

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-// Purpose: Main file for the app. Contains the DebateTimer component.
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './styles/App.css';
-import './DebateTimer'
-import DebateTimer from "./DebateTimer";
+import DebateTimer from './DebateTimer';
+import SettingsPage from './DebateSetting'; // 新增的设置页面组件
 
 function App() {
-  return (
-    <div className="App">
-      <DebateTimer />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                {/* 导航栏 */}
+                <nav>
+                    <Link to="/">Debate Timer</Link> |
+                    <Link to="/settings">Settings</Link>
+                </nav>
+
+                {/* 路由配置 */}
+                <Routes>
+                    <Route path="/" element={<DebateTimer />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;

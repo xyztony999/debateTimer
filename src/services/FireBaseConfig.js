62e470f55d9a70b04ec009056e84327f751d43f9
Xinyi Zhang <42613048+xyztony999@users.noreply.github.com>
2026-05-09 22:10:08 +0800

    fix: refactor components and improve performance by optimizing imports and using useCallback for memoization

--- src/services/FireBaseConfig.js
+++ src/services/FireBaseConfig.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+getAnalytics(app);
 const db = getFirestore(app);
 const database = getDatabase(app);
 
